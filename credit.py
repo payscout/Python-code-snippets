aabfa49f46dea027ccda5d3af12aeb48a46c4c58
Payscout Inc <victor@payscout.com>
2017-11-08 16:51:24 +0200

    Update credit.py

--- credit.py
+++ credit.py
@@ -2,7 +2,7 @@ import requests
 
 url = "https://gatewaystaging.payscout.com/api/process"
 
-payload = "client_username={yourUsername}&client_password={yourPassword}&client_token=token&processing_type=CREDIT&expiration_month=10&expiration_year=2022&account_number={yourTestCardNumber}&cvv2=123¤cy=USD&initial_amount=99.99"
+payload = "client_username={yourUsername}&client_password={yourPassword}&client_token=token&processing_type=CREDIT&expiration_month=10&expiration_year=2022&account_number={yourTestCardNumber}&cvv2=123&currency=USD&initial_amount=99.99"
 headers = {
     'content-type': "application/x-www-form-urlencoded",
     'cache-control': "no-cache"
