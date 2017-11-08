68398242fd8f1188aee0c39d8b079b6a42016a31
Payscout Inc <victor@payscout.com>
2017-11-08 16:50:57 +0200

    Update capture.py

--- capture.py
+++ capture.py
@@ -2,7 +2,7 @@ import requests
 
 url = "https://gatewaystaging.payscout.com/api/process"
 
-payload = "client_username={yourUsername}&client_password={yourPassword}&client_token=token&processing_type=CAPTURE¤cy=USD&initial_amount=99.99&original_transaction_id=A000133RY3R&billing_invoice_number=1999"
+payload = "client_username={yourUsername}&client_password={yourPassword}&client_token=token&processing_type=CAPTURE&currency=USD&initial_amount=99.99&original_transaction_id=A000133RY3R&billing_invoice_number=1999"
 headers = {
     'content-type': "application/x-www-form-urlencoded",
     'cache-control': "no-cache"
