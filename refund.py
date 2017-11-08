1064d93650460e4ddf5830cd0c80c9f8765a0468
Payscout Inc <victor@payscout.com>
2017-11-08 16:51:38 +0200

    Update refund.py

--- refund.py
+++ refund.py
@@ -2,7 +2,7 @@ import requests
 
 url = "https://gatewaystaging.payscout.com/api/process"
 
-payload = "client_username={yourUsername}&client_password={yourPassword}&client_token=token&processing_type=REFUND¤cy=USD&initial_amount=99.99&original_transaction_id=A0001FFCDJ9"
+payload = "client_username={yourUsername}&client_password={yourPassword}&client_token=token&processing_type=REFUND&currency=USD&initial_amount=99.99&original_transaction_id=A0001FFCDJ9"
 headers = {
     'content-type': "application/x-www-form-urlencoded",
     'cache-control': "no-cache"
